46046731c97b415433ac2c86c45e82c3283abad2
ShalOCal <shamullen2@yahoo.co.uk>
2023-02-18 09:51:48 +0000

    adding functions for player choice and winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,38 @@
 
 function playGame() {
 
-}
 
-function whichClick() {
 
-}
+    function whichClick() {
 
-function computerMove() {
+    }
 
-}
+    function computerMove() {
+        
+    }
 
-function decideWinner() {
+    function decideWinner(p1, p2) {
+        let result = null;
+      
+        switch (p1.choice + p2.choice) {
+          case "rockscissors":
+          case "scissorspaper":
+          case "paperrock":
+            result = p1;
+            break;
+          case "scissorsrock":
+          case "paperscissors":
+          case "rockpaper":
+            result = p2;
+            break;
+          case "paperpaper":
+          case "scissorsscissors":
+          case "rockrock":
+            result = null;
+            break;
+        }
+        return result;
+      };
 
 }
 
